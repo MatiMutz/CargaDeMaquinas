08b3fe3e2ce8d6bc284e6652afb7bf4068ca2298
Mati Mutz <matias.mutz@gmail.com>
2020-11-03 18:20:53 -0300

    Ver plano y ver Programa (pdf y txt)

--- SupplyChain/Controllers/ControllerSolution.cs
+++ SupplyChain/Controllers/ControllerSolution.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using SupplyChain;
+using Microsoft.Extensions.Configuration;
 
 namespace SupplyChain
 {
@@ -20,104 +23,18 @@ namespace SupplyChain
             _context = context;
         }
 
-        // GET: api/Solution
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Solution>>> GetSolution()
+        public IEnumerable<Solution> Get(string CAMPO)
         {
-            return await _context.Solution.ToListAsync();
-        }
-
-        // GET: api/Solution/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Solution>> GetSolution(int CAMPO)
-        {
-            var Solution = await _context.Solution.FindAsync(CAMPO);
-
-            if (Solution == null)
-            {
-                return NotFound();
-            }
-
-            return Solution;
-        }
-
-        // PUT: api/Solution/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutSolution(string Id, Solution Solution)
-        {
-            if (Id != Solution.CAMPO)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(Solution).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
+                string xSQL = string.Format("select CAMPO, VALORC from Solution where CAMPO = 'OFRUTA1' OR CAMPO = 'CNCRUTA'");
+                return _context.Solution.FromSqlRaw(xSQL).ToList<Solution>();
             }
-            catch (DbUpdateConcurrencyException)
+            catch
             {
-                if (!SolutionExists(Id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return new List<Solution>();
             }
-
-            return NoContent();
-        }
-
-        // POST: api/Solution
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
-        [HttpPost]
-        public async Task<ActionResult<Solution>> PostSolution(Solution Solution)
-        {
-            _context.Solution.Add(Solution);
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
-            {
-                if (SolutionExists(Solution.CAMPO))
-                {
-                    return Conflict();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return CreatedAtAction("GetSolution", new { id = Solution.CAMPO }, Solution);
-        }
-
-        // DELETE: api/Solution/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<Solution>> DeleteSolution(string CAMPO)
-        {
-            var Solution = await _context.Solution.FindAsync(CAMPO);
-            if (Solution == null)
-            {
-                return NotFound();
-            }
-
-            _context.Solution.Remove(Solution);
-            await _context.SaveChangesAsync();
-
-            return Solution;
-        }
-
-        private bool SolutionExists(string CAMPO)
-        {
-            return _context.Solution.Any(e => e.CAMPO == CAMPO);
         }
     }
 }
