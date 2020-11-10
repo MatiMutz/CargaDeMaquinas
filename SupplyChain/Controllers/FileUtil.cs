ea0035f6b812075d517e27ac81b08dc2372fde55
Willy <gonzalobrizuelaortiz@icloud.com>
2020-11-10 16:19:11 -0300

    export pdf prueba

---
+++ SupplyChain/Controllers/FileUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.JSInterop;
+
+namespace SupplyChain
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public static class FileUtil
+    {
+        public static ValueTask<object> SaveAs(this IJSRuntime js, string filename, byte[] data)
+            => js.InvokeAsync<object>(
+                "saveAsFile",
+                filename,
+                Convert.ToBase64String(data));
+    }
+
+}
