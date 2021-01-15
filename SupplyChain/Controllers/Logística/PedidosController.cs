abf14eb23fb8315f422d0a997ac9738093222cdc
Mati Mutz <matias.mutz@gmail.com>
2021-01-15 17:01:16 -0300

    15-1-21 Empezando pantalla de Trazabilidad

--- SupplyChain/Controllers/Logística/PedidosController.cs
+++ SupplyChain/Controllers/Logística/PedidosController.cs
@@ -27,8 +27,50 @@ namespace SupplyChain
         [HttpGet]
         public IEnumerable<Pedidos> Get(string PEDIDO)
         {
-            string xSQL = string.Format("SELECT Pedidos.REGISTRO, Pedidos.PEDIDO, Pedidos.REMITO, Pedidos.FLAG, Pedidos.CG_ORDF, Pedidos.TIPOO, Pedidos.CG_TIRE FROM((Pedcli INNER JOIN Programa ON Pedcli.PEDIDO = Programa.PEDIDO) INNER JOIN Pedidos ON pedcli.PEDIDO = Pedidos.PEDIDO) where(pedidos.FLAG = 0 AND Programa.CG_ESTADO = 3 AND Pedidos.CG_ORDF != 0 AND(Pedidos.TIPOO = 1)) UNION SELECT Pedidos.REGISTRO, Pedidos.PEDIDO, Pedidos.REMITO, Pedidos.FLAG, Pedidos.CG_ORDF, Pedidos.TIPOO, Pedidos.CG_TIRE FROM((Pedcli INNER JOIN Programa ON Pedcli.PEDIDO = Programa.PEDIDO) INNER JOIN Pedidos ON pedcli.PEDIDO = Pedidos.PEDIDO) where Pedcli.PEDIDO NOT IN(select PEDIDO from Pedidos where TIPOO = 1) AND Programa.CG_ESTADO = 3  AND Pedcli.CANTPED > 0 AND Pedidos.TIPOO != 28");
+            string xSQL = string.Format("SELECT Pedidos.REGISTRO, Pedidos.PEDIDO, Pedidos.REMITO, Pedidos.FLAG, Pedidos.CG_ORDF, Pedidos.TIPOO, Pedidos.CG_TIRE, Pedido.DES_CLI FROM((Pedcli INNER JOIN Programa ON Pedcli.PEDIDO = Programa.PEDIDO) INNER JOIN Pedidos ON pedcli.PEDIDO = Pedidos.PEDIDO) where(pedidos.FLAG = 0 AND Programa.CG_ESTADO = 3 AND Pedidos.CG_ORDF != 0 AND(Pedidos.TIPOO = 1)) UNION SELECT Pedidos.REGISTRO, Pedidos.PEDIDO, Pedidos.REMITO, Pedidos.FLAG, Pedidos.CG_ORDF, Pedidos.TIPOO, Pedidos.CG_TIRE, Pedidos.DES_CLI FROM((Pedcli INNER JOIN Programa ON Pedcli.PEDIDO = Programa.PEDIDO) INNER JOIN Pedidos ON pedcli.PEDIDO = Pedidos.PEDIDO) where Pedcli.PEDIDO NOT IN(select PEDIDO from Pedidos where TIPOO = 1) AND Programa.CG_ESTADO = 3  AND Pedcli.CANTPED > 0 AND Pedidos.TIPOO != 28");
             return _context.Pedidos.FromSqlRaw(xSQL).ToList<Pedidos>();
         }
+
+        // GET:
+        [HttpGet("BuscarTrazabilidad/{Pedido}/{Cliente}/{Busqueda}")]
+        public async Task<ActionResult<List<Pedidos>>> BuscarTrazabilidad(string Pedido, string Cliente, int Busqueda)
+        {
+            List<Pedidos> lContiene = new List<Pedidos>();
+            if (Cliente == "Vacio")
+            {
+                if (_context.Pedidos.Any())
+                {
+                    lContiene = await _context.Pedidos.Where(p => p.PEDIDO.ToString().Contains(Pedido)).OrderByDescending(s => s.PEDIDO).Take(Busqueda).ToListAsync();
+                }
+                if (lContiene == null)
+                {
+                    return NotFound();
+                }
+            }
+            else if (Pedido == "Vacio")
+            {
+                if (_context.Pedidos.Any())
+                {
+                    lContiene = await _context.Pedidos.Where(p => p.DES_CLI.Contains(Cliente)).OrderByDescending(s => s.PEDIDO).Take(Busqueda).ToListAsync();
+                }
+                if (lContiene == null)
+                {
+                    return NotFound();
+                }
+            }
+            else if (Pedido != "Vacio" && Cliente != "Vacio")
+            {
+                if (_context.Pedidos.Any())
+                {
+                    lContiene = await _context.Pedidos.Where(p => p.PEDIDO.ToString().Contains(Pedido) && p.DES_CLI.Contains(Cliente)).OrderByDescending(s => s.PEDIDO).Take(Busqueda).ToListAsync();
+                }
+                if (lContiene == null)
+                {
+                    return NotFound();
+                }
+            }
+            return lContiene;
+        }
+
     }
 }
